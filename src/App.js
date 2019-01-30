882f0764e1f173f46a567192b19dd11983f4ccd0
GermainDuran <arturodurantorres@gmail.com>
2019-01-30 13:19:01 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+
+state = {
+  data: []
+}
+
+
+   componentDidMount() {
+
+     fetch('http://localhost:3000/api/v1/users').then(resp => resp.json()).then(data => this.setState({
+      data: data
+     })).then(() => console.log(this.state))
+  }
+
+
   render() {
     return (
       <div className="App">
